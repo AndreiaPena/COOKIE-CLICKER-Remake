d3c8ddf4f557fe0895b94046fb388302c9dea96b
Samira TAJIK <samiratajik.dm@gmail.com>
2020-04-29 11:43:41 +0200

    cree class bakery

---
+++ js/classes/bakery.mjs
@@ -0,0 +1,10 @@
+class Bakery{
+    constructor(name, cookies, buildings, cookiesPerClick, cookiesPerSecond){
+        this.name = name;
+        this.cookies = cookies;
+        this.buildings = buildings;
+        this.cookiesPerClick = cookiesPerClick;
+        this.cookiesPerSecond = cookiesPerSecond;
+    }
+    
+}
