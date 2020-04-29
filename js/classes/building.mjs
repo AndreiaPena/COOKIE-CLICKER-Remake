a587abfcfa4de967286db6dcf8fa789743f9f769
Coline <colineleroy@gmail.com>
2020-04-29 11:33:29 +0200

    class building

---
+++ js/classes/building.mjs
@@ -0,0 +1,8 @@
+class building {
+    constructor(name, description, number, cookiesPerSecond, cost)
+        this.name = name
+        this.description = description
+        this.number = number
+        this.cookiesPerSecond = cookiesPerSecond
+        this.cost = cost
+}
